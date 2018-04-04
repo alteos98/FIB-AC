3486aa81f9fe9259e341b73212530f7916d18dae
Albert Teira Osuna <33811344+alteos98@users.noreply.github.com>
2018-04-04 22:32:10 +0200

    Add files via upload

--- Lab/Practiques/Practica5/MiSimulador.c
+++ Lab/Practiques/Practica5/MiSimulador.c
@@ -1,22 +1,31 @@
 #include "CacheSim.h"
+#include <stdio.h>
 
 /* Posa aqui les teves estructures de dades globals
  * per mantenir la informacio necesaria de la cache
  * */
-
+unsigned int cache[128];
+unsigned int ocupat[128]; //ens servirÃ  per saber si hem de reemplaÃ§ar o no
+int h; //nÂº hit
+int m; //nÂº miss
 
 
 
 /* La rutina init_cache es cridada pel programa principal per
  * inicialitzar la cache.
- * La cache es inicialitzada al començar cada un dels tests.
+ * La cache es inicialitzada al comenï¿½ar cada un dels tests.
  * */
 void init_cache ()
 {
     totaltime=0.0;
 	/* Escriu aqui el teu codi */
+	
+	h = 0;
+  	m = 0;
 
-
+	for (int i = 0; i < 128; ++i) { //posem totes les lÃ­nies com a no ocupades
+		ocupat[i] = 0;
+	}
 
 }
 
@@ -28,30 +37,35 @@ void reference (unsigned int address)
 	unsigned int linea_mc;
 	unsigned int tag;
 	unsigned int miss;	   // boolea que ens indica si es miss
-	unsigned int replacement;  // boolea que indica si es reemplaça una linia valida
-	unsigned int tag_out;	   // TAG de la linia reemplaçada
+	unsigned int replacement;  // boolea que indica si es reemplaï¿½a una linia valida
+	unsigned int tag_out;	   // TAG de la linia reemplaï¿½ada
 	float t1,t2;		// Variables per mesurar el temps (NO modificar)
 	
 	t1=GetTime();
 	/* Escriu aqui el teu codi */
+	byte = address & 0x1F; // 5 bits
+	bloque_m = address >> 5;
+	linea_mc = bloque_m & 0x7F; // 7 bits
+	tag = bloque_m >> 7; // 20 bits
+	
+	miss = !ocupat[linea_mc] || cache[linea_mc] != tag;
+	
+	replacement = miss && ocupat[linea_mc];
+	if (replacement) tag_out = cache[linea_mc];
+	
+	cache[linea_mc] = tag;
+	ocupat[linea_mc] = 1;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	if (miss) {
+		++m;
+	}
+	else {
+		++h;
+	}
 
 	/* La funcio test_and_print escriu el resultat de la teva simulacio
 	 * per pantalla (si s'escau) i comproba si hi ha algun error
-	 * per la referencia actual. També mesurem el temps d'execució
+	 * per la referencia actual. Tambï¿½ mesurem el temps d'execuciï¿½
 	 * */
 	t2=GetTime();
 	totaltime+=t2-t1;
@@ -63,6 +77,6 @@ void reference (unsigned int address)
 void final ()
 {
  	/* Escriu aqui el teu codi */ 
-  
+  	printf("Hits: %d, miss: %d\n", h, m);
   
 }
